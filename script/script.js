1d4cbab141c86b394e212622e091e57dd2138f5b
AqulaR <nikolayashapatov@gmail.com>
2024-03-04 23:34:25 +0300

    last commit

--- script/script.js
+++ script/script.js
@@ -1,29 +1,50 @@
-let badi = document.getElementsByClassName('badi')[0];
-let all_modals = document.getElementsByClassName('all_modals');
+let badi = document.getElementsByClassName("badi")[0];
+let all_modals = document.getElementsByClassName("all_modals");
 
 function open_modal(elem) {
     for (let i = 0; i < all_modals.length; i++) {
-        all_modals[i].style.display = 'none';
+        all_modals[i].style.display = "none";
     }
-    modal_now = document.querySelectorAll(`div#${elem.dataset.target}.all_modals`)[0];
-    modal_now.style.display = 'flex';
-    badi.style.overflow = 'hidden';
+    modal_now = document.querySelectorAll(
+        `div#${elem.dataset.target}.all_modals`
+    )[0];
+    modal_now.style.display = "flex";
+    badi.style.overflow = "hidden";
 }
 
 function close_modal(elem) {
-    modal_now = document.querySelectorAll(`div#${elem.dataset.target}.all_modals`)[0];
-    modal_now.style.display = 'none';
-    badi.style.overflow = 'visible';
+    modal_now = document.querySelectorAll(
+        `div#${elem.dataset.target}.all_modals`
+    )[0];
+    modal_now.style.display = "none";
+    badi.style.overflow = "visible";
 }
 
-document.addEventListener('click', function (evt) {
-
+document.addEventListener("click", function (evt) {
     // Закрытие модалок на 500
     for (let i = 0; i < all_modals.length; i++) {
         if (evt.target === all_modals[i]) {
-            all_modals[i].style.display = 'none';
-            badi.style.overflow = 'visible';
+            all_modals[i].style.display = "none";
+            badi.style.overflow = "visible";
         }
     }
-
 });
+
+// accordion
+
+let acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        this.children[1].classList.toggle("rotated");
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            setTimeout(() => panel.classList.remove("mt-3"), 200);
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.classList.add("mt-3");
+        }
+    });
+}
